cac307c82dd89e9443c77d4e6a0430e6e4365ac5
Mohamed Mostafa Abdelrahim Mohamed Elsayed <118544654+momo285@users.noreply.github.com>
2024-06-24 00:28:13 +0300

    Add files via upload

---
+++ precache-manifest.e27e3ac097cbe4f9082a25dd0a0b08c4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "308467a61ef7604e542d059ee56745ee",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f9125a8b1c4a7b9470a3",
+    "url": "/portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "e9ee0f07ad41723b0703",
+    "url": "/portfolio/static/css/main.045c0826.chunk.css"
+  },
+  {
+    "revision": "f9125a8b1c4a7b9470a3",
+    "url": "/portfolio/static/js/2.550c4eb2.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/portfolio/static/js/2.550c4eb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9ee0f07ad41723b0703",
+    "url": "/portfolio/static/js/main.5809997b.chunk.js"
+  },
+  {
+    "revision": "6c972d9418aa5b4e59ab",
+    "url": "/portfolio/static/js/runtime-main.1b893ad1.js"
+  }
+]);
